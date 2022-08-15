57d45f9f8b47e45f5c6cad777e40e737ec2e39f9
Your Name <you@example.com>
2022-08-15 04:24:00 -0700

    5555

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import web3 from './Component/web3/web3'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
