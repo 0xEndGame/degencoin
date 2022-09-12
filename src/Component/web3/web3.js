aacea0f39b22de7d622b9f9521cacb7b51a4deeb
Your Name <you@example.com>
2022-09-12 19:04:00 +0200

    2222

--- src/Component/web3/web3.js
+++ src/Component/web3/web3.js
@@ -1,9 +1,12 @@
 import Web3 from "web3";
      
+async function connectWallet() {
+
+
 window.ethereum.request({ method: "eth_requestAccounts" });
  
 const web3 = new Web3(window.ethereum);
 
-
+}
  
 export default web3;
