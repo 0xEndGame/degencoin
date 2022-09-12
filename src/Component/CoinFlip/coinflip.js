cb9fb56280c5628fe91732e2a2ca7267d53f6290
Your Name <you@example.com>
2022-09-12 21:34:53 +0200

    2222

---
+++ src/Component/CoinFlip/coinflip.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Button, Image, Modal } from "react-bootstrap";
+import MainCoin from "../../Assets/Images/main-coin.png";
+import Coin from "../../Assets/Images/coin-icon-01.jpg";
+import "./coinflip.css";
+import { Link } from "react-router-dom";
+import Web3 from "web3";
+
+const CoinFlip = () => {
+
+
+}
