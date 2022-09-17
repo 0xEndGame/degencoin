66f4245c467aeffb707612b9d2f5617f8eccbf47
Your Name <you@example.com>
2022-09-17 15:55:54 +0200

    22

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import CoinWallet from "./Component/Coinwallet/coinwallet";
+import CoinConnect from "./Component/CoinConnect/CoinConnect";
+import CoinFlip from "./Component/CoinFlip/CoinFlip";
+import Header from "./Component/Header/header";
+import Footer from "./Component/Footer/Footer";
+import web3 from './Component/web3/web3'
+import multitest from './Component/web3/multitest'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={ <CoinWallet /> } />
+        <Route path="/coin-connect" element={ <CoinConnect/> } />
+        <Route path="/coinflip" element={ <CoinFlip/> } />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+  
+}
+
+export default App;
---
+++ src/Component/CoinFlip/CoinFlip.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Button, Image, Modal } from "react-bootstrap";
+import MainCoin from "../../Assets/Images/main-coin.png";
+import Coin from "../../Assets/Images/coin-icon-01.jpg";
+import "./coinflip.css";
+import { Link } from "react-router-dom";
+import Web3 from "web3";
+
+const CoinFlip = () => {
+
+return(
+<div class="btn-group">
+  <button>0.05 ETH</button>
+  <button>0.1 ETH </button>
+  <button>0.25 ETH</button>
+</div>
+);
+};
+
+export default CoinFlip;
