6546f44a8f0297ea3e26f9890ca08031400c8b61
Your Name <you@example.com>
2022-10-20 21:35:12 +0200

    44

--- src/Component/CoinFlip/CoinFlip.js
+++ src/Component/CoinFlip/CoinFlip.js
@@ -5,6 +5,8 @@ import Coin from "../../Assets/Images/coin-icon-01.jpg";
 import "./coinflip.css";
 import { Link } from "react-router-dom";
 import Web3 from "web3";
+import multiepoch from "../web3/multitest";
+
 
 const CoinFlip = () => {
 
@@ -12,9 +14,22 @@ const walletConnector = () => {
   window.ethereum.request({ method: "eth_requestAccounts" });
 };
 
+const getData = () => {
+  multiepoch.methods.epoch().call(function (err,res) {
+    if (err) {
+      console.log("an error occured", err)
+      return
+    }
+    console.log("the epoch is: ", res)
+  })
+  }
+};
+
 return(
 <div class="btn-group">
   <button id="walletConnect" onClick={walletConnector}>Connect your wallet you degen</button>
+
+  <button id="scTest" onClick={getData}> Request Data</button>
 
 </div>
 
